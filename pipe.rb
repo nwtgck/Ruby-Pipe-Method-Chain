a69e8f9ae92b0b5573bb84d37b101fd468f36d7e
Ryo Ota <nwtgck@gmail.com>
2015-05-13 21:45:34 +0900

    [change] Symbolクラスの@@sym2params, params, clear_paramsを削除して、[]がSymMethodのインスタンスを返すようにした [debug] 1 ^ 1 ^ :putsが「1」と表示されずにエラーになったので、修正

--- pipe.rb
+++ pipe.rb
@@ -7,44 +7,37 @@
 # 例えば、「"%f" % 0.12012」にrjustするとき、全体をカッコで囲み「("%f" % 0.12012)」と書き換えなくてはいけなくなります
 # そして、それを出力しようと思ったら、行頭に戻って puts を書く必要がありますが、「^ :puts」と書くだけでOKです
 
-class Symbol
-	# シンボルから引数を取り出すために
-	@@sym2params = Hash.new([])
-
-	# method(10)が:method[10]
-	def [] *params
-		@@sym2params[self] += params
-		self
-	end
-
-	# シンボルに渡されている引数を返す
-	def params
-		@@sym2params[self]
-	end
-
-	# シンボルに登録されている引数を削除
-	def clear_params
-		@@sym2params[self] = []
+# メソッド名と引数を管理するため
+class SymMethod
+	attr_reader :name, :params
+	def initialize(name, params)
+		@name, @params = name, params
 	end
 end
+
+# シンボル[]をした時にSymMethodを返すようにする
+class Symbol
+	# method(10)が:method[10]
+	def [] *params
+		SymMethod.new(self, params)
+	end
+end
+
 
 # すべてのクラスに適応する
 Object.constants.select{|c| Object.const_get(c).class == Class}.map{|c| Object.const_get(c)}.each do |clazz|
 	clazz.class_eval do
 		# 「|」あるときはbarに退避させる
 		alias_method(:bar, :|) if method_defined?(:|)
-		alias_method(:hat, :"^") if method_defined?(:"^") 
+		alias_method(:hat, :'^') if method_defined?(:'^') 
 
 		# self.method が self | :methodで動くように
 		def | (method, *others)
 			# method名がシンボルなら
 			if method.class == Symbol
-				# パラメータを読み込んでメソッドを呼ぶ
-				res = self.send(method, *method.params)
-				# シンボルに登録されているparamsを削除する
-				method.clear_params
-				# 結果を返す
-				res
+				self.send(method)
+			elsif method.class == SymMethod
+				self.send(method.name, *method.params)
 			else
 				# method名がシンボルでないなら、普通の「|」を呼ぶ
 				bar(method, *others) if self.class.method_defined? :bar
@@ -53,8 +46,17 @@ Object.constants.select{|c| Object.const_get(c).class == Class}.map{|c| Object.c
 
 		# puts "hello"を "hello" ^ :putsで動かす
 		def ^(func)
-			send(func, self) if self.class.method_defined? :hat
+			if func.class == Symbol
+				send(func, self) if self.class.method_defined? :hat
+			else
+				send(:hat, func) if self.class.method_defined? :hat
+			end
+			
 		end
 
 	end
 end
+
+
+# p *1..10] | :size
+1 ^ 1 ^ :puts
